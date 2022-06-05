fc7c369f18e8ea5667cfbd930fff3d98eddf2774
HappyRogelio7 <58347721+HappyRogelio7@users.noreply.github.com>
2022-06-04 22:37:36 -0400

    Update MessageColors.java

--- MessageColors.java
+++ MessageColors.java
@@ -4,7 +4,7 @@ import org.bukkit.Bukkit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MessagesUtils {
+public class MessageColors {
 
     public static String getMsgColor(String text) {
 
