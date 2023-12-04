0304052869b7ecfc53cf6731738ad9e497d925d2
HappyRogelio7 <58347721+HappyRogelio7@users.noreply.github.com>
2023-12-04 02:51:04 -0300

    1.20.2 and 1.20.3 Support | Update MessageColors.java

--- MessageColors.java
+++ MessageColors.java
@@ -24,6 +24,7 @@ public class MessageColors {
                 || Bukkit.getVersion().contains("1.19.1") || Bukkit.getVersion().contains("1.19.2")
                 || Bukkit.getVersion().contains("1.19.3") || Bukkit.getVersion().contains("1.19.4")
                 || Bukkit.getVersion().contains("1.20") || Bukkit.getVersion().contains("1.20.1") 
+                || Bukkit.getVersion().contains("1.20.2") || Bukkit.getVersion().contains("1.20.3")
         ) {
             Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
             Matcher matcher = pattern.matcher(text);
