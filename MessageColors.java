a11f5b28891cc006747f8dc0f872f3546c7b5007
Happy7 <58347721+Happyuky7@users.noreply.github.com>
2025-10-09 11:47:57 -0300

    Add support for Bukkit version 1.21.9 and 1.21.10

--- MessageColors.java
+++ MessageColors.java
@@ -31,6 +31,7 @@ public class MessageColors {
                 || Bukkit.getVersion().contains("1.21.3") || Bukkit.getVersion().contains("1.21.4")
                 || Bukkit.getVersion().contains("1.21.5") || Bukkit.getVersion().contains("1.21.6")
                 || Bukkit.getVersion().contains("1.21.7") || Bukkit.getVersion().contains("1.21.8")
+                || Bukkit.getVersion().contains("1.21.9") || Bukkit.getVersion().contains("1.21.10")
         ) {
 
             Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
